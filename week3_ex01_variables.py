9e6f7054f299cda428678ac1428e1504b5a2c419
Thabo Joseph Banda <tb3508771@gmail.com>
2026-04-22 10:27:49 +0200

    completed variables exercise

---
+++ week3_ex01_variables.py
@@ -0,0 +1,13 @@
+name = "Thabo"
+Age = "20"
+print("Hello I am",name ,"I am",Age,  "years old")
+
+length = int(6)
+width = int(3)
+Area =(length*width)
+print (Area)
+
+C = float(25.5)
+
+f = ((C*9/5)+32)
+print(round(f,2))
