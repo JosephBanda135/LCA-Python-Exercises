c4bceb0c76a2f1057183a202777eb97882fc3332
Thabo Joseph Banda <tb3508771@gmail.com>
2026-04-24 20:29:22 +0200

    Add Exercise 04 - Loops and Modules

---
+++ week3_ex04_loops_modules.py
@@ -0,0 +1,34 @@
+#Question 1
+
+fruits = ["apple", "banana" , "orange" , "mango" , "watermelon"]
+
+for fruit in fruits:
+  print(fruit)
+
+#Question 2  
+count = 5
+while count >0:
+  print(count)
+  count -=1
+
+#Question 3
+
+for i in range (1,11):
+  print(i*i)
+
+#Question 4
+
+import random
+
+colours = ["red" , "blue" , "green" , "yellow" , "purple" , "orange"]
+
+for i in range (3):
+  print(random.choice(colours))
+
+#Question 5
+
+import math_operations
+
+while True:
+  print (math_operations.multiply(2,3))
+  break
